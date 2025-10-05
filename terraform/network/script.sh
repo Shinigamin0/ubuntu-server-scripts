acc0815f3bca324b7e29dce039eda4c0a28f1441
Shinigamin0 <omar.vargas.cardenas@gmail.com>
2025-10-05 10:43:06 +0000

    ajuste

--- terraform/network/script.sh
+++ terraform/network/script.sh
@@ -1,6 +1,6 @@
 terraform fmt
 terraform init -reconfigure -backend-config=backend.hcl
-terraform plan/applY/destro \
+terraform plan/apply/destroy \
   -var='azs=["us-east-1a","us-east-1b"]' \
   -var='public_cidrs=["10.0.0.0/24","10.0.1.0/24"]' \
   -var='private_cidrs=["10.0.10.0/24","10.0.11.0/24"]' \
